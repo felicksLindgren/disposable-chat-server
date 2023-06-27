1b5818ded755f0f13ba05034920d57b928b1fce7
Felix Lindgren <felix.p.lindgren@gmail.com>
2023-06-27 08:28:43 +0000

    adding code

--- index.js
+++ index.js
@@ -1,12 +1,65 @@
-const http = require('http');
-const PORT = 3000;
+import { Server } from "socket.io";
+import * as eiows from "eiows";
+import rug from "random-username-generator";
+import * as dotenv from 'dotenv';
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+dotenv.config();
+
+const io = new Server(process.env.PORT, {
+    wsEnginge: eiows.Server,
+    cors: {
+        origin: process.env.CLIENT_URL,
+    }
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+io.use((socket, next) => {
+    rug.setSeperator('_');
+    let username = rug.generate();
+    username = username.split('_').slice(0, 2).join('_').toLocaleLowerCase();
+    socket.username = username;
+
+    next();
+});
+
+io.on("connection", async (socket) => {
+    const room = socket.handshake.query.code || socket.id;
+
+    console.log(socket.username, "joining room", room);
+    socket.join(room);
+
+    // emit all users in the room
+    const users = [];
+
+    await io.in(room).fetchSockets().then((sockets) => {
+        sockets.forEach((socket) => {
+            users.push({
+                userID: socket.id,
+                username: socket.username
+            });
+        });
+    });
+
+    socket.emit("users", users);
+
+    socket.on("message", (message) => {
+        console.log(socket.username, message, room);
+        const message_to_emit = {
+            userID: socket.id,
+            username: socket.username,
+            message,
+            time: new Date().toLocaleTimeString()
+        };
+
+        io.in(room).emit("message", message_to_emit);
+    });
+
+    // emit new user
+    socket.to(room).emit("user connected", {
+        userID: socket.id,
+        username: socket.username
+    });
+
+    socket.on("disconnect", () => {
+        socket.to(room).emit("user disconnected", socket.id);
+    });
 });
